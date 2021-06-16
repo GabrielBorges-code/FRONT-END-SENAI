293d9ce4c8ee88ecc544b07de6683ff5666f1e9b
Gabriel B <gabriel.moura2009@hotmail.com>
2021-06-16 11:11:58 -0300

    ativida 10 finalizada

---
+++ Atividades/atividade-10/ex1.js
@@ -0,0 +1,61 @@
+var nome = "Gabriel Borges";
+var idade = "22";
+var matricula = "0009814";
+var prova1 = 7;
+var prova2 = 8;
+var mediaProva = (prova1 + prova2) / 2;
+var numCelular = "985830508";
+var temMais9digitos = numCelular.length > 9 ? "Sim" : "Não";
+var elevado = Math.pow(32, 6);
+
+// Atividade 1
+console.log("O arquivo 'ex1.js' está funcionando");
+
+// Atividade 2
+console.log("Olá, eu me chamo " + nome +  " tenho " + idade + " anos e estou estudando Javascript");
+
+// Atividade 3
+console.log("O aluno " + nome + ", matrícula " + matricula + ", obteve a média final " + mediaProva);
+
+// Atividade 4
+console.log("O telefone '" + numCelular + "' celular tem mais de 9 dígitos? " + temMais9digitos);
+
+// Atividade 5
+console.log("O numero 32 elevado a 6 " + elevado);
+
+console.log("-----------------")
+// Atividade 6
+
+var $num1;
+// var typeof;
+// var nome-aluno;
+var tentativa_2;
+// var 2a_tentativa;
+// var nome completo;
+
+console.log("-----------------")
+// Atividade 7
+var quantidade = "25";
+var numero = 6;
+var pressao;
+var temperatura = 40;
+temperatura = null;
+
+console.log(quantidade += quantidade);
+console.log( (7+5) / numero + 2 ); 
+console.log(pressao);
+console.log(temperatura);
+console.log(typeof pressao);
+console.log(typeof temperatura);
+
+console.log("-----------------")
+// Atividade 8
+var idade = 65;
+
+console.log(idade != 65);
+console.log(idade >= 65); 
+console.log("65" == idade);
+console.log(65 !== idade);
+console.log(typeof (idade > 60));
+console.log(typeof (idade > 70));
+
